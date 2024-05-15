11eaa5ccde5c53abb3e7b37f6447483e1e7e11e3
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-15 10:54:19 +0800

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
+  const [numberPlus, setNumberPlus] = useState<number>(0)
+
+  const handlePlus = () => {
+    try {
+      setNumberPlus(prevNumber => prevNumber + 1);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleMinus = () => {
+    try {
+      if (numberPlus == 0) {
+        return
+      } else {
+        setNumberPlus(prevNumber => prevNumber - 1);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='title'>COUNT</span>
+      <div className='content'>
+        <button className='btn-minus' onClick={handleMinus}>-</button>
+        <span className='hasil'>{numberPlus}</span>
+        <button className='btn-plus' onClick={handlePlus}>+</button>
+      </div>
     </div>
   );
 }
